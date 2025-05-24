44e31f47c9b6c2025b7c1e924824940737886cf5
kalilwiz <leokalil100@gmail.com>
2025-05-23 21:56:05 -0300

    atualizacao validators

--- Senai notes/Validators/UsuarioValidator.cs
+++ Senai notes/Validators/UsuarioValidator.cs
@@ -1,6 +1,23 @@
-﻿namespace Senai_notes.Validators
+﻿using FluentValidation;
+using Senai_notes.Dtos;
+
+namespace Senai_notes.Validators
 {
-    public class UsuarioValidator
+    //  herdando a classe abstractValidator do pacote e usando a interface do metodo cadastrar usuario
+    public class UsuarioValidator : AbstractValidator<UsuarioDto>
     {
+        //  criando um construtor para criar e ler as regras
+        public UsuarioValidator()
+        {
+
+            //  criando regra para o nome
+            //  NotEmpty - nao deixa ser vazio
+            //  maximumlength - indica o maximo de caracteres
+            //  withmessage - tras uma mensagem para quando nao atender a regra
+            RuleFor(u => u.Nome)
+            .NotEmpty().WithMessage("O nome de Usuario e obrigatorio").MaximumLength(100).WithMessage("O nome de usuario deve ter no maximo 100 caracteres");
+
+
+        }
     }
 }
